85d33a8d256edb55c3ea2153cb5a4f2162cc259f
Alhrafas <abbadegugger@gmail.com>
2023-07-22 16:37:13 +0100

    initial commit

--- base/views.py
+++ base/views.py
@@ -77,7 +77,7 @@ def home(request):
             Q(description__icontains=q)
             
             )  
-      topics = Topic.objects.all()
+      topics = Topic.objects.all()[0:5]
       room_count = room.count()
       room_message = Message.objects.filter(Q(room__topic__name__icontains=q))
       
@@ -196,3 +196,14 @@ def updateUser(request):
       
       context = {'form': form}
       return render(request, 'base/update-user.html', context)
+
+
+def topicPage(request):
+      q = request.GET.get('q') if request.GET.get('q') != None else ''
+      topics = Topic.objects.filter(name__icontains=q)
+      
+      return render(request, 'base/topics.html', {'topics': topics})
+
+def activityPage(request):
+      room_messages = Message.objects.all()
+      return render(request, 'base/activity.html', {'room_messages': room_messages})
